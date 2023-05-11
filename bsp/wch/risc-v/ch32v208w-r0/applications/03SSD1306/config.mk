4fbc2b2994d936eb73240cc1ff8cb3bfba373cbe
rx-ted <1552488649@qq.com>
2023-05-11 22:13:41 +0800

    update :The current date is: Thu 05/11/2023  Enter the new date: (mm-dd-yy)

---
+++ bsp/wch/risc-v/ch32v208w-r0/applications/03SSD1306/config.mk
@@ -0,0 +1,29 @@
+BSP_ROOT ?= ../..
+RTT_ROOT ?= ../../../../../..
+
+
+CFLAGS := -march=rv32imac -mabi=ilp32 -DUSE_PLIC -DUSE_M_TIME -DNO_INIT -mcmodel=medany -msmall-data-limit=8 -L.  -nostartfiles  -lc  -save-temps=obj -O0 -g3
+AFLAGS :=-c -march=rv32imac -mabi=ilp32 -DUSE_PLIC -DUSE_M_TIME -DNO_INIT -mcmodel=medany -msmall-data-limit=8 -L.  -nostartfiles  -lc  -x assembler-with-cpp -I. -g3
+LFLAGS := -march=rv32imac -mabi=ilp32 -DUSE_PLIC -DUSE_M_TIME -DNO_INIT -mcmodel=medany -msmall-data-limit=8 -L.  -nostartfiles  -lc  -Wl,--gc-sections,-cref,-Map=$(BUILD_DIR)/$(TARGET).map -T $(BSP_ROOT)/board/linker_scripts/link.lds
+EXTERN_LIB := -lc -lm 
+
+CPPPATHS +=-I.
+CPPPATHS +=-I$(BSP_ROOT)/applications 
+CPPPATHS +=-I$(RTT_ROOT)/bsp/wch/risc-v/Libraries/ch32v20x_libraries/bmsis 
+CPPPATHS +=	-I$(RTT_ROOT)/bsp/wch/risc-v/Libraries/ch32v20x_libraries/bmsis/include 
+CPPPATHS +=	-I$(RTT_ROOT)/bsp/wch/risc-v/Libraries/ch32v20x_libraries/ch32v208_hal 
+CPPPATHS +=	-I$(RTT_ROOT)/bsp/wch/risc-v/Libraries/ch32v20x_libraries/ch32v208_hal/include 
+CPPPATHS +=	-I$(RTT_ROOT)/components/libc/compilers/common/include 
+CPPPATHS +=	-I$(RTT_ROOT)/components/libc/compilers/newlib 
+CPPPATHS +=	-I$(RTT_ROOT)/libcpu/risc-v/common 
+CPPPATHS +=	-I$(RTT_ROOT)/components/drivers/include 
+CPPPATHS +=	-I$(BSP_ROOT)/board 
+CPPPATHS +=	-I$(RTT_ROOT)/bsp/wch/risc-v/Libraries/ch32_drivers 
+CPPPATHS +=	-I$(RTT_ROOT)/components/finsh 
+CPPPATHS +=	-I$(BSP_ROOT) 
+CPPPATHS +=	-I$(RTT_ROOT)/include 
+CPPPATHS +=	-I$(RTT_ROOT)/components/libc/posix/io/poll 
+CPPPATHS +=	-I$(RTT_ROOT)/components/libc/posix/io/stdio 
+CPPPATHS +=	-I$(RTT_ROOT)/components/libc/posix/ipc 
+
+DEFINES := -DRT_USING_LIBC -DRT_USING_NEWLIBC -D_POSIX_C_SOURCE=1 -D__RTTHREAD__
